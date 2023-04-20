4656b0c729d560f720840f3d1524c07b41ed70ac
CarlosDoncel27 <79527521+CarlosDoncel27@users.noreply.github.com>
2023-04-20 14:19:31 -0500

    Add files via upload

---
+++ ej02/conos.js
@@ -0,0 +1,65 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xDDDDDD, 1);
+document.body.appendChild(renderer.domElement);
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(80, WIDTH / HEIGHT);
+camera.position.z = 24.5;
+camera.position.x = -1.2;
+camera.position.y = 2;
+
+camera.rotation.set(0, -0.5, 0);
+scene.add(camera);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+
+//CONO
+
+const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+Sx = 1;
+
+Sy =  0.5;
+
+Sz = 3;
+
+
+function Traslacion(vt) {
+  var matriz = new THREE.Matrix4();
+  matriz.set(1, 0, 0, vt[0],
+          0, 1, 0, vt[1],
+          0, 0, 1, vt[2],
+          0, 0, 0, 1);
+  return matriz;
+}
+
+
+
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+const size = 150;
+const divisions = 160;
+const axesHelper = new THREE.AxesHelper(1000);
+scene.add(axesHelper);
+
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+render();
